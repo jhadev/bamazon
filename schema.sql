0a9f83e08a15ad9042debea5dca841c56f8db04e
JA <joshappeldev@gmail.com>
2018-10-19 10:17:33 -0400

    schema seeds

--- schema.sql
+++ schema.sql
@@ -12,3 +12,32 @@ CREATE TABLE products(
   PRIMARY KEY (id)
 );
 
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Bonk's Adventure", "Video Games", 400, 2);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Breaking Bad The Complete Series", "Television", 50, 39);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Leather Jacket", "Clothing", 260, 18);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Soft and Luxurious Dress Socks", "Clothing", 32, 320);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Apple Watch", "Electronics", 300, 4);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Red Dead Redemption 2", "Video Games", 60, 14);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Better Call Saul Season 1", "Television", 20, 4);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Bamazon Balexa NSA Microwave", "Electronics", 70, 600);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Kranzjams Vol: 443", "Music", 10, 21);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Kranzjams Vol: 1", "Music", 20, 2);
